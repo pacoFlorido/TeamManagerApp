48ee2711cd60b35ae27dcbd3cb28b95348b7b0de
fflorido <fflorido@devcenter.es>
2024-05-25 20:07:13 +0200

    Layout crear usuario creado.

--- app/src/main/java/net/pacofloridoquesada/teammanager/ui/register/UserCreationFragment.kt
+++ app/src/main/java/net/pacofloridoquesada/teammanager/ui/register/UserCreationFragment.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.navArgs
 import net.pacofloridoquesada.teammanager.R
-import net.pacofloridoquesada.teammanager.databinding.FragmentRoleBinding
 import net.pacofloridoquesada.teammanager.databinding.FragmentUserCreationBinding
 
 class UserCreationFragment : Fragment() {
@@ -15,6 +15,13 @@ class UserCreationFragment : Fragment() {
     private var _binding: FragmentUserCreationBinding? = null
     private val binding get() = _binding!!
     val args: UserCreationFragmentArgs by navArgs()
+
+    private fun inicializarSpinnerPaises(){
+        val adapter = ArrayAdapter.createFromResource(this.requireContext(), R.array.paises , R.layout.spinner_items)
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown)
+
+        binding.spPaises.adapter = adapter
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +35,8 @@ class UserCreationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.ejemplo.text = args.roleSelected.toString()
+
+        this.inicializarSpinnerPaises()
     }
 
     override fun onDestroyView() {
